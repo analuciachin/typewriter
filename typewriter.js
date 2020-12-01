8768d37ec4393bb7c92ac383d7f9bee8ea90f721
analuciachin <ananime@gmail.com>
2020-12-01 14:53:27 +0000

    implemented the type writer logic and output a newline caracter at the end of the sentence

--- typewriter.js
+++ typewriter.js
@@ -1,40 +1,22 @@
 const sentence = "hello there from lighthouse labs";
 
-//console.log('chatAt:  ', sentence.charAt(0));
-
-// setTimeout(() => {
-//   for (const char of sentence) {
-//     process.stdout.write(char);
-//   }
-// }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
-
-// setTimeout(() => {
-//   for (const char of sentence) {
-//     process.stdout.write(char);
-//   }
-// }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
-
 delay = 1000;
-let i;
+let i = 0;
+
 for(const char of sentence) {
-  i++;
   delay += 300;
   setTimeout(() => {
-    process.stdout.write(char);
+    if (i < sentence.length - 1) {
+      process.stdout.write(char);
+      i++;
+    } else {
+      process.stdout.write(char);
+      process.stdout.write('\n');
+    }
+    
   }, delay);
 }
-if (i === sentence.length) {
-  console.log(' ');
-}
 
-// setTimeout(() => {
-//   console.log(sentence.charAt(0));
-// }, 1000);
 
-// setTimeout(() => {
-//   console.log(sentence.charAt(1));
-// }, 1500);
 
-// setTimeout(() => {
-//   console.log(sentence.charAt(2));
-// }, 2000);
+
